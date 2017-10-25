ad94530defd77370b73e3042b1d8db11e7701c53
Marek Suchánek <marek.suchanek@gmail.com>
2017-10-25 09:14:28 +0200

    Adding setup/module tests

---
+++ tests_module/conftest.py
@@ -0,0 +1,135 @@
+import configparser
+import os
+import pytest
+import subprocess
+
+
+ABS_PATH = os.path.abspath(os.path.dirname(__file__))
+FIXTURES_PATH = ABS_PATH + '/fixtures'
+
+
+class MissingConfigError(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class Utils:
+
+    def __init__(self, cfg, tmpdir, sh):
+        self.cfg = cfg
+        self.tmpdir = tmpdir
+        self.sh = sh
+
+    @property
+    def git(self):
+        return self.cfg['commands']['git']
+
+    @property
+    def create_venv(self):
+        return self.cfg['commands']['create_venv']
+
+    @property
+    def python(self):
+        return str(self.tmpdir.join(self.cfg['commands']['python']))
+
+    @property
+    def pytest(self):
+        return '{} -m {}'.format(self.python, self.cfg['commands']['pytest'])
+
+    @property
+    def pip(self):
+        return '{} -m {}'.format(self.python, self.cfg['commands']['pip'])
+
+    @property
+    def pip_testpypi(self):
+        return '{} -m {}'.format(self.python, self.cfg['commands']['pip_testpypi'])
+
+    @staticmethod
+    def ssh_github(reposlug):
+        return 'git@github.com:{}.git'.format(reposlug)
+
+    @staticmethod
+    def https_github(reposlug):
+        return 'https://github.com/{}.git'.format(reposlug)
+
+    @property
+    def repo_ssh(self):
+        return self.cfg['vars']['repo_full']
+
+    @property
+    def repo_branch(self):
+        return self.cfg['vars']['branch']
+
+    def get_set(self, set_name):
+        return frozenset(self.cfg.get('sets', set_name, fallback='').split(' '))
+
+    def create_fresh_venv(self):
+        result = self.sh(self.create_venv)
+        assert result.was_successful, \
+            'Could not create virtualenv for Python: {}'.format(result.stderr)
+
+    def clone_repo(self, repo_dir):
+        result = self.sh(self.git, 'clone', '-b', self.repo_branch, self.repo_ssh, repo_dir)
+        assert result.was_successful, \
+            'Could not clone the repository {}: {}'.format(self.repo_ssh, result.stderr)
+
+    def clone_repo_with_fresh_venv(self, repo_dir):
+        self.create_fresh_venv()
+        self.clone_repo(repo_dir)
+
+
+class ShellExecutionResult:
+    def __init__(self, stdout, stderr, return_code):
+        self.stdout = stdout
+        self.stderr = stderr
+        self.return_code = return_code
+
+    @property
+    def was_successful(self):
+        return self.return_code == 0
+
+    @property
+    def outerr(self):
+        return '{}\n{}\n{}'.format(self.stdout, '-'*80, self.stderr)
+
+
+@pytest.fixture()
+def config():
+    ext_vars = {
+        'LABELORD_BRANCH': 'master'
+    }
+    ext_vars.update(os.environ)
+
+    cfg = configparser.ConfigParser(ext_vars)
+    cfg.read(FIXTURES_PATH + '/test_config.cfg')
+
+    if not cfg.has_option('vars', 'ctu_username'):
+        raise MissingConfigError(
+            'CTU_USERNAME env var is missing!'
+        )
+    if not cfg.has_option('vars', 'repo_full'):
+        raise MissingConfigError(
+            'LABELORD_REPO env var is missing!'
+        )
+
+    return cfg
+
+
+@pytest.fixture()
+def sh():
+    def shell_executor(command, *args):
+        p = subprocess.Popen(
+            ' '.join([command, *args]),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            universal_newlines=True
+        )
+        stdout, stderr = p.communicate()
+        return ShellExecutionResult(stdout, stderr, p.returncode)
+    return shell_executor
+
+
+@pytest.fixture()
+def utils(config, tmpdir, sh):
+    return Utils(config, tmpdir, sh)
---
+++ tests_module/test_distribution.py
@@ -0,0 +1,102 @@
+import email
+import pkg_resources
+import tarfile
+import glob
+import os
+
+
+def test_sdist(utils, tmpdir, sh):
+    tmpdir.chdir()  # Work in separate tmp dir
+
+    repo_dir = 'repo'
+    # Prepare venv and clone repository to repo dir
+    utils.clone_repo_with_fresh_venv(repo_dir)
+
+    # Sdist with setup.py
+    tmpdir.join(repo_dir).chdir()
+    result = sh(utils.python, 'setup.py', 'sdist')
+    assert result.was_successful, \
+        'Could not sdist via setup.py: {}'.format(result.stderr)
+
+    assert 'warning' not in result.outerr, \
+        'There are some warnings in sdist output'
+
+    # Check content of distributed .tar.gz file
+    tmpdir.join(repo_dir).join('dist').chdir()
+    dist_tgz_files = glob.glob('*.tar.gz')
+    assert len(dist_tgz_files) != 0, \
+        'No dist .tar.gz file has been produced'
+    distfile = dist_tgz_files[0]
+
+    tar_dist = tarfile.open(distfile)
+    counts = {
+        'license': 0,
+        'template_html': 0,
+        'readme': 0
+    }
+    for info in tar_dist.getmembers():
+        if not info.isfile():
+            continue
+        path, file = os.path.split(info.name)
+        if file in utils.get_set('license'):
+            if info.size > 0:
+                counts['license'] += 1
+        elif file in utils.get_set('readme'):
+            if info.size > 0:
+                counts['readme'] += 1
+        elif file.endswith('.html') and path.endswith('templates'):
+            # Sorry if you are using some custome name for templates dir
+            if info.size > 0:
+                counts['template_html'] += 1
+
+    # There should be exactly one non-empty license info file
+    assert counts['license'] > 0, \
+        'No LICENSE/COPYING file provided in distributed .tar.gz'
+    assert counts['license'] == 1, \
+        'Multiple LICENSE/COPYING files provided in distributed .tar.gz'
+
+    # There should be exactly one non-empty readme file
+    assert counts['readme'] > 0, \
+        'No README(.md,.rst) file provided in distributed .tar.gz'
+    assert counts['readme'] == 1, \
+        'Multiple README(.md,.rst) files provided in distributed .tar.gz'
+
+    # There should be some HTML templates for web app
+    assert counts['template_html'] > 0, \
+        'No web app templates included in distributed .tar.gz'
+
+
+def test_package_info(utils, tmpdir, sh):
+    tmpdir.chdir()  # Work in separate tmp dir
+
+    repo_dir = 'repo'
+    # Prepare venv and clone repository to repo dir
+    utils.clone_repo_with_fresh_venv(repo_dir)
+
+    # Sdist with setup.py
+    tmpdir.join(repo_dir).chdir()
+    result = sh(utils.python, 'setup.py', 'install')
+    assert result.was_successful, \
+        'Could not sdist via setup.py: {}'.format(result.stderr)
+    tmpdir.chdir()
+
+    # Read package metadata
+    pkg_info = pkg_resources.get_distribution('labelord').get_metadata('PKG-INFO')
+    info_items = email.message_from_string(pkg_info).items()
+
+    classifiers = [x[1] for x in info_items if x[0] == 'Classifier']
+    assert len(classifiers) > 4, 'Need to have at least 5 classifiers'
+    assert 'Framework :: Flask' in classifiers
+    assert 'Environment :: Console' in classifiers
+    assert 'Environment :: Web Environment' in classifiers
+
+    mandatory = ['Author', 'Author-email', 'License', 'Name', 'Summary',
+                 'Description', 'Keywords']
+    for m in mandatory:
+        found = False
+        for item in info_items:
+            if item[0] == m:
+                found = True
+                assert len(item[1]) > 0, 'Metadata {} is empty'.format(m)
+                break
+        assert found, 'Metadata {} is not present'.format(m)
