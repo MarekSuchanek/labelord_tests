MarekSuchanek/labelord_tests

commit 934b93161f8fe27eed298e8e6506a2348f21a229

Use self.pip instead of a hardcoded value
Should not make any difference, but just to stay consistent

tests_module/conftest.py
@@ -81,7 +81,7 @@ class Utils:
         result = self.sh(self.create_venv)
         assert result.was_successful, \
             'Could not create virtualenv for Python: {}'.format(result.stderr)
-        self.sh(self.python, '-m', 'pip', 'install', '--upgrade', 'setuptools')
+        self.sh(self.python, '-m', self.pip, 'install', '--upgrade', 'setuptools')
 
     def clone_repo(self, repo_dir):
         result = self.sh(self.git, 'clone', '-b', self.repo_branch, self.repo_ssh, repo_dir)

commit a9e55bd0da088212f83546fa91a0f04a988d421a

Fix bug introduced by 934b931

tests_module/conftest.py
@@ -81,7 +81,7 @@ class Utils:
         result = self.sh(self.create_venv)
         assert result.was_successful, \
             'Could not create virtualenv for Python: {}'.format(result.stderr)
-        self.sh(self.python, '-m', self.pip, 'install', '--upgrade', 'setuptools')
+        self.sh(self.pip, 'install', '--upgrade', 'setuptools')
 
     def clone_repo(self, repo_dir):
         result = self.sh(self.git, 'clone', '-b', self.repo_branch, self.repo_ssh, repo_dir)